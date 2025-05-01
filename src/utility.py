b2f01da6188328499ffc0c4e6e0ae1452b5649a5
aManWithHand <88043496+aManWithHand@users.noreply.github.com>
2025-05-01 11:29:59 +0700

    Update utility.py

--- src/utility.py
+++ src/utility.py
@@ -1,4 +1,18 @@
+import os
+import pygame
+
+
 class Utillity:
     @staticmethod
-    def load():
-        pass
+    def load_png(name: str) -> pygame.Surface | pygame.Rect:
+        fullname = os.path.join("data", name)
+        try:
+            image = pygame.image.load(fullname)
+            if image.get_alpha() is None:
+                image = image.convert()
+            else:
+                image = image.convert_alpha()
+        except FileNotFoundError:
+            print(f"Cannot load image: {fullname}")
+            raise SystemExit
+        return image, image.get_rect()
