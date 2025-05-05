111f41ca78e753538a5aa1d0303e9321b9645c1c
aManWithHand <88043496+aManWithHand@users.noreply.github.com>
2025-05-05 11:25:08 +0700

    Update throwable_object.py

--- src/throwable_object.py
+++ src/throwable_object.py
@@ -1,10 +1,15 @@
 from pygame.sprite import Sprite
 from pygame.math import Vector2
+import pygame
+import utility as util
 
 class ThrowableObject(Sprite):
-    def __init__(self, *groups):
+    def __init__(self, image: str, speed: tuple = (0,0), *groups):
         super().__init__(*groups)
-        self.a = Vector2
+        self.speed = Vector2(speed)
+        self.image, self.rect = util.load_png(image)
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
 
     def update(self, *args, **kwargs):
         pass
